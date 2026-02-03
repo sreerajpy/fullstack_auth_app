c6cefba6b5c68e8703319ab7351971296537353c
sreeraj <sreeraj.kr@m2comsys.com>
2026-02-03 14:52:45 +0530

    env commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,5 @@ require("dotenv").config();
 
 const app = require('./app')
 
-app.listen(5000, () => {
-    console.log("server running port 5000");
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on", PORT));
